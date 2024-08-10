4188678e49f9810388164f22557ce025ecb5fb51
Lyna F. ElNaffar <67642540+Lyna-NA@users.noreply.github.com>
2024-08-10 20:11:56 +0100

    Update database.ts

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,12 +1,17 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from 'sequelize';
 
-const db = new Sequelize({
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "P@s$123$",
-    database: "Balls_DB",
-    dialect: "postgres",
+// Replace with your Render PostgreSQL connection URL
+const databaseUrl = 'postgresql://balls_user:7P4HR0Hvh3mrjhgwblnnRSse4OcE892T@dpg-cqron808fa8c73d7sbk0-a.oregon-postgres.render.com/balls';
+
+const db = new Sequelize(databaseUrl, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Set to true if you have a valid SSL certificate
+    }
+  },
+  logging: false, // Set to true if you want to see SQL queries in the console
 });
 
 export default db;
