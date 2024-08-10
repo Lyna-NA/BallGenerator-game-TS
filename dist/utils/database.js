64527ad456c576744eb4b0b7a797dac0b65370ad
Lyna-NA <lyna.naf88@gmail.com>
2024-08-10 20:29:04 +0100

    commit

--- dist/utils/database.js
+++ dist/utils/database.js
@@ -1,12 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const db = new sequelize_1.Sequelize({
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "P@s$123$",
-    database: "Balls_DB",
-    dialect: "postgres",
+// Replace with your Render PostgreSQL connection URL
+const databaseUrl = 'postgresql://balls_user:7P4HR0Hvh3mrjhgwblnnRSse4OcE892T@dpg-cqron808fa8c73d7sbk0-a.oregon-postgres.render.com/balls';
+const db = new sequelize_1.Sequelize(databaseUrl, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false // Set to true if you have a valid SSL certificate
+        }
+    },
+    logging: false, // Set to true if you want to see SQL queries in the console
 });
 exports.default = db;
