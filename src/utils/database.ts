8ea09d17a955bf2819dae1b1efe4e3b455070776
Lyna-NA <lyna.naf88@gmail.com>
2024-08-10 20:44:53 +0100

    commit

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,17 +1,21 @@
 import { Sequelize } from 'sequelize';
 
-// Replace with your Render PostgreSQL connection URL
-const databaseUrl = 'postgresql://balls_user:7P4HR0Hvh3mrjhgwblnnRSse4OcE892T@dpg-cqron808fa8c73d7sbk0-a.oregon-postgres.render.com/balls';
-
-const db = new Sequelize(databaseUrl, {
+const db = new Sequelize('postgresql://balls_user:7P4HR0Hvh3mrjhgwblnnRSse4OcE892T@dpg-cqron808fa8c73d7sbk0-a.oregon-postgres.render.com/balls', {
   dialect: 'postgres',
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false // Set to true if you have a valid SSL certificate
+      rejectUnauthorized: false,
     }
-  },
-  logging: false, // Set to true if you want to see SQL queries in the console
+  }
 });
+
+db.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 export default db;
