17cb6f34d36b189444ed0f56b90a71aabde67de9
Lyna-NA <lyna.naf88@gmail.com>
2024-08-10 20:48:56 +0100

    commit

--- dist/utils/database.js
+++ dist/utils/database.js
@@ -1,16 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-// Replace with your Render PostgreSQL connection URL
-const databaseUrl = 'postgresql://balls_user:7P4HR0Hvh3mrjhgwblnnRSse4OcE892T@dpg-cqron808fa8c73d7sbk0-a.oregon-postgres.render.com/balls';
-const db = new sequelize_1.Sequelize(databaseUrl, {
+const db = new sequelize_1.Sequelize('postgresql://balls_user:7P4HR0Hvh3mrjhgwblnnRSse4OcE892T@dpg-cqron808fa8c73d7sbk0-a.oregon-postgres.render.com/balls', {
     dialect: 'postgres',
     dialectOptions: {
         ssl: {
             require: true,
-            rejectUnauthorized: false // Set to true if you have a valid SSL certificate
+            rejectUnauthorized: false,
         }
-    },
-    logging: false, // Set to true if you want to see SQL queries in the console
+    }
+});
+db.authenticate()
+    .then(() => {
+    console.log('Connection has been established successfully.');
+})
+    .catch(err => {
+    console.error('Unable to connect to the database:', err);
 });
 exports.default = db;
